d70f3b9dbb8b013379a60ad0422291b1cbd71ea0
rajesh096 <151764875+rajesh096@users.noreply.github.com>
2024-08-22 09:14:19 +0530

    Time: 581 ms (5.03%), Space: 37.6 MB (5.4%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,18 +1,13 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         dic = {k:v for k,v in (zip(set(nums),[[] for i in range(len(nums))]))}
-        res=[]
         for i in range(len(nums)):
             dic[nums[i]].append(i)
         for i,j in dic.items():
             if len(j)>1:
-                a=0
                 for x in range(len(j)-1):
                     a=abs(j[x]-j[x+1])
                     if a<=k:
                         return True
         return False
-        '''if len(res)==0 or max(res)>k:
-            return False
-        else:
-            return True'''
+        
