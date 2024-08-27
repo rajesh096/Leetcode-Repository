519601c4f99720cc07f303064a1f183ab4d79c8f
rajesh096 <151764875+rajesh096@users.noreply.github.com>
2024-08-27 20:58:39 +0530

    Time: 2684 ms (5.01%), Space: 18.3 MB (28.46%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -7,16 +7,9 @@ class Solution:
         re1=sorted(res)
         print(res)
         r=[]
-        for i in re1[:k]:
-            n=res.index(i)
+        for i in range(k):
+            n=res.index(re1[i])
             res[n]=-1
             r.append(arr[n])
-        # dic=dict(sorted(dic.items(), key=lambda item: item[1]))
-        # for i,j in dic.items():
-        #     if k!=0:
-        #         res.append(i)
-        #         k-=1
-        #     else:
-        #         break
         
         return sorted(r)
