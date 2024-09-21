f55070415d0f2c7111136ea6fb08d48395cb8534
rajesh096 <151764875+rajesh096@users.noreply.github.com>
2024-09-21 15:12:23 +0530

    Time: 223 ms (53.34%), Space: 18 MB (69.05%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -3,6 +3,7 @@ class Solution:
         left = 0
         right = len(arr) - 1
 
+
         while right - left + 1 > k: 
             if abs(arr[left] - x) > abs(arr[right] - x):
                 left += 1
