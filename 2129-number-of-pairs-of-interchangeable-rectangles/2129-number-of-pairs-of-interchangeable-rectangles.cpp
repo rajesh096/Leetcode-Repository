fcc63304e2c8e0210e3c30939b33623831b32c8f
Rajesh <151764875+rajesh096@users.noreply.github.com>
2024-11-04 21:13:22 +0530

    Time: 160 ms (28.75%), Space: 161.3 MB (11.27%) - LeetHub

---
+++ 2129-number-of-pairs-of-interchangeable-rectangles/2129-number-of-pairs-of-interchangeable-rectangles.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& rectangles) {
+        long long res=0;
+        unordered_map<double,int> dic;
+        for(vector<int> i:rectangles){
+            double x = static_cast<double>(i[0]) / static_cast<double>(i[1]);
+            dic[x]+=1;
+        }
+        for(auto i=dic.begin();i!=dic.end();i++){
+            long long a=i->second;
+            // cout<<i->first;
+            res+=(a*(a-1))/2;
+        }
+        return res;
+    }
+};
