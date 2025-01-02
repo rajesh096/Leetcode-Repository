d98a6dedac82040a0faa765f80497f7270e2873e
Rajesh <151764875+rajesh096@users.noreply.github.com>
2025-01-02 12:08:16 +0530

    Time: 17 ms (75.27%), Space: 49.7 MB (47.74%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.py
@@ -1,10 +1,10 @@
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        arr=[]
+        arr=[0]
         r=0
         vo=['a','e','i','o','u']
         for i in words:
-            if(i[0] in vo):
+            if(i[0] in vo) and (i[-1] in vo):
                 r+=1
                 arr.append(r)
             else:
@@ -12,13 +12,13 @@ class Solution:
         res=[]
         print(arr)
         for a,b in queries:
-            if(a==0):
-                res.append(arr[b])
-            elif(b-a==0):
-                if(words[1][0] in vo):
-                    res.append(1)
-                else:
-                    res.append(0)
-            else:
-                res.append(arr[b]-arr[a])
+            
+            res.append(arr[b+1]-arr[a])
+            # elif(b-a==0):
+            #     if(words[1][0] in vo):
+            #         res.append(1)
+            #     else:
+            #         res.append(0)
+            # else:
+            #     res.append(arr[b]-arr[a])
         return res
