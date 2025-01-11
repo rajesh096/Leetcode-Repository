ac382682932d1d966db8fefae7cd38fc40466efe
Rajesh <151764875+rajesh096@users.noreply.github.com>
2025-01-11 09:31:25 +0530

    Time: 35 ms (5.59%), Space: 15 MB (33.92%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool canConstruct(string s, int k) {
-        map<char,int> mp;
         if(s.size()<k) return false;
+        map<char,int> mp;
         for(char i : s) mp[i]++;
         int odd = 0;
         for(const auto& pair : mp){
             if(pair.second%2==1) odd++;
         }
-        
         if(odd>k) return false;
         return true;
     }
