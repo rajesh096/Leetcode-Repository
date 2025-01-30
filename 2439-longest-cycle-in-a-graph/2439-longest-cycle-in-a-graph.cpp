2258f67607ca84dc6bd394dfa3bbf2339ef8ae6b
Rajesh <151764875+rajesh096@users.noreply.github.com>
2025-01-30 21:07:23 +0530

    Time: 11 ms (97.07%), Space: 94.8 MB (95.21%) - LeetHub

--- 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.cpp
@@ -1,52 +1,18 @@
 class Solution {
 public:
-    long long dfs(int id, const vector<vector<int>>& adj, vector<int>& par, vector<int>& temp, vector<int>& vis, int p = -1) {
-        vis[id] = 1;
-        par[id] = p;
-        temp.push_back(id);
-        for (auto it : adj[id]) {
-            if (!vis[it]) {
-                long long z = dfs(it, adj, par, temp, vis, id);
-                if (z != -1) {
-                    return z;
-                }
-            } else if (vis[it] == 1) {
-                long long s = 1;
-                while (id != it) {
-                    s++;
-                    id = par[id];
-                }
-                return s;
-            }
-        }
-        return -1;
-    }
-
     int longestCycle(vector<int>& edges) {
-        int N = edges.size();
-        vector<vector<int>> adj(N);
-        vector<int> par(N, -1);
-        vector<int> vis(N, 0);
-        long long res = -1; 
+        int n = edges.size(), l = -1;
+        vector<int> visit(n, 0);
 
-        for (int i = 0; i < N; i++) {
-            if (edges[i] != -1) {
-                adj[i].push_back(edges[i]);
+        for (int i = 0, time = 1, j, curr_time; i < n; i++) {
+            j = i, curr_time = time;
+            while (j != -1 && !visit[j]) {
+                visit[j] = time++;
+                j = edges[j];
             }
+            if (j != -1 && visit[j] >= curr_time) l = max(l, time - visit[j]);
         }
 
-        for (int i = 0; i < N; i++) {
-            if (!vis[i]) {
-                vector<int> temp;
-                long long result = dfs(i, adj, par, temp, vis);
-                if (result != -1) {
-                    res = max(res, result);
-                }
-                for (auto it : temp) {
-                    vis[it] = 2;
-                }
-            }
-        }
-        return res;
+        return l;
     }
 };
